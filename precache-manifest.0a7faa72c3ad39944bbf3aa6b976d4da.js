d755789d4c52553d2bd3f0d99ff518a64f7a6260
shaundre1 <swhyte18@my.centennialcollege.ca>
2021-05-30 22:02:39 -0400

    Updates

---
+++ precache-manifest.0a7faa72c3ad39944bbf3aa6b976d4da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf21efa39428ed88c338",
+    "url": "/static/css/main.d8fc8077.chunk.css"
+  },
+  {
+    "revision": "cf21efa39428ed88c338",
+    "url": "/static/js/main.cf21efa3.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c14f8909ff5f0715cdf916d3da6a6ed1",
+    "url": "/index.html"
+  }
+];
